05580f7d76759faf176174c63a541932b7411a50
Ben Noble <ben.noble@thoughtworks.com>
2020-12-02 14:27:45 +0000

    [DayTwoTaskTwo] BN - CharaterCount factory does the string manuipulation now

--- AdventOfCode2020.App/CharacterCountPolicyFactory.cs
+++ AdventOfCode2020.App/CharacterCountPolicyFactory.cs
@@ -2,9 +2,15 @@ namespace AdventOfCode2020.App
 {
     public class CharacterCountPolicyFactory : IPasswordPolicyFactory
     {
-        public IPasswordPolicy CreateFromString(string input)
+        public IPasswordPolicy CreateFromString(string policy)
         {
-            return new CharacterCountPolicy(input);
+            var splitBySpace = policy.Split(' ');
+            var range = splitBySpace[0];
+            var character = splitBySpace[1][0];
+            var rangeSplitByDash = range.Split("-");
+            var minAppearances = int.Parse(rangeSplitByDash[0]); 
+            var maxAppearances = int.Parse(rangeSplitByDash[1]);
+            return new CharacterCountPolicy(character, minAppearances, maxAppearances);
         }
     }
 }
