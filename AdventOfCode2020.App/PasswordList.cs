465390b80a2c2b7686a663b026ce530ec04f3357
Ben Noble <ben.noble@thoughtworks.com>
2020-12-02 12:54:16 +0000

    [DayTwoTaskOne] BN - Can create password lists from files

---
+++ AdventOfCode2020.App/PasswordList.cs
@@ -0,0 +1,25 @@
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2020.App
+{
+    public class PasswordList
+    {
+        private readonly Password[] _passwords;
+        public PasswordList(params Password[] passwords)
+        {
+            _passwords = passwords;
+        }
+
+        public PasswordList(string filePath)
+        {
+            var lines = File.ReadAllLines(filePath);
+            _passwords = lines.Select(line => new Password(line)).ToArray();
+        }
+
+        public int NumValid()
+        {
+            return _passwords.Count(p => p.IsValid());
+        }
+    }
+}
---
+++ AdventOfCode2020.Test/PasswordListTest.cs
@@ -0,0 +1,26 @@
+using AdventOfCode2020.App;
+using NUnit.Framework;
+
+namespace AdventOfCode2020.Test
+{
+    [TestFixture]
+    public class PasswordListTest
+    {
+        [Test]
+        public void CountMultipleCorrectPasswords()
+        {
+            var password1 = new Password("abcde", new PasswordPolicy('a', 1, 3));
+            var password2 = new Password("cdefg", new PasswordPolicy('b', 1, 3));
+            var password3 = new Password("ccccccccc", new PasswordPolicy('c', 1, 9));
+            var list = new PasswordList(password1, password2, password3);
+            Assert.That(list.NumValid(), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void CountValidPasswordsInFile()
+        {
+            var list = new PasswordList("TestFiles/DayTwoExample.txt");
+            Assert.That(list.NumValid(), Is.EqualTo(2));
+        }
+    }
+}
