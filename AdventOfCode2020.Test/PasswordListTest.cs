7ec18e06ee39e4f001b25d2483baee1b6d93a947
Ben Noble <ben.noble@thoughtworks.com>
2020-12-02 14:09:55 +0000

    [DayTwoTaskTwo] BN - Testing password list can cope with different policies

--- AdventOfCode2020.Test/PasswordListTest.cs
+++ AdventOfCode2020.Test/PasswordListTest.cs
@@ -12,8 +12,11 @@ namespace AdventOfCode2020.Test
             var password1 = new Password("abcde", new CharacterCountPolicy('a', 1, 3));
             var password2 = new Password("cdefg", new CharacterCountPolicy('b', 1, 3));
             var password3 = new Password("ccccccccc", new CharacterCountPolicy('c', 1, 9));
-            var list = new PasswordList(password1, password2, password3);
-            Assert.That(list.NumValid(), Is.EqualTo(2));
+            var password4 = new Password("abcde", new CharacterPositionPolicy('a', 1, 3));
+            var password5 = new Password("cdefg", new CharacterPositionPolicy('b', 1, 3));
+            var password6 = new Password("ccccccccc", new CharacterPositionPolicy('c', 1, 9));
+            var list = new PasswordList(password1, password2, password3, password4, password5, password6);
+            Assert.That(list.NumValid(), Is.EqualTo(3));
         }
 
         [Test]
