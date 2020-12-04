0f4e71258d24f335ec8df87ecda25413c0fe12c9
Ben Noble <ben.noble@thoughtworks.com>
2020-12-04 08:09:14 +0000

    [DayFourTaskTwo] BN - Refactored so Passport extents NorthPoleCredential

--- AdventOfCode2020.App/PassportControl/Passport.cs
+++ AdventOfCode2020.App/PassportControl/Passport.cs
@@ -2,38 +2,19 @@ using System;
 
 namespace AdventOfCode2020.App.PassportControl
 {
-    public class Passport : IIdentityCredential
+    public class Passport : NorthPoleCredentials
     {
-        private readonly int _birthYear;
-        private readonly int _issueYear;
-        private readonly int _expirationYear;
-        private readonly string _height;
-        private readonly string _hairColor;
-        private readonly string _eyeColor;
-        private readonly string _id;
         private readonly string _countryId;
 
         public Passport(int birthYear, int issueYear, int expirationYear, string height, string hairColor, string eyeColor, string id, string countryId)
+        : base(birthYear, issueYear, expirationYear, height, hairColor, eyeColor, id)
         {
-            _birthYear = birthYear;
-            _issueYear = issueYear;
-            _expirationYear = expirationYear;
-            _height = height;
-            _hairColor = hairColor;
-            _eyeColor = eyeColor;
-            _id = id;
             _countryId = countryId;
         }
 
         protected bool Equals(Passport other)
         {
-            return _birthYear == other._birthYear && 
-                   _issueYear == other._issueYear && 
-                   _expirationYear == other._expirationYear && 
-                   _height == other._height && 
-                   _hairColor == other._hairColor && 
-                   _eyeColor == other._eyeColor && 
-                   _id == other._id && 
+            return base.Equals(other) &&
                    _countryId == other._countryId;
         }
 
@@ -47,18 +28,12 @@ namespace AdventOfCode2020.App.PassportControl
 
         public override int GetHashCode()
         {
-            return HashCode.Combine(_birthYear, _issueYear, _expirationYear, _height, _hairColor, _eyeColor, _id, _countryId);
+            return HashCode.Combine(base.GetHashCode(), _countryId);
         }
 
         public override string ToString()
         {
-            return $"byr:{_birthYear} " +
-                   $"iyr:{_issueYear} " +
-                   $"eyr:{_expirationYear} " +
-                   $"hgt:{_height} " +
-                   $"hcl:{_hairColor} " +
-                   $"ecl:{_eyeColor} " +
-                   $"pid:{_id} " +
+            return base.ToString() + " " +
                    $"cid:{_countryId}";
         }
     }
