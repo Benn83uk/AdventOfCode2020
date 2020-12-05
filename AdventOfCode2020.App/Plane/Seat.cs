5bd429946a1f3da90549da014da847b098203c62
Ben Noble <ben.noble@thoughtworks.com>
2020-12-05 15:00:38 +0000

    [DayFive] BN - Refactored to remove duplicate code in Row() and Column()

--- AdventOfCode2020.App/Plane/Seat.cs
+++ AdventOfCode2020.App/Plane/Seat.cs
@@ -25,18 +25,20 @@ namespace AdventOfCode2020.App.Plane
 
         public int Row()
         {
-            var rowAsBinaryString = _row
-                .Replace("F", "0")
-                .Replace("B", "1");
-            return Convert.ToInt32(rowAsBinaryString, 2);
+            return FromStringWithBinary(_row, "F", "B");
         }
 
         public int Column()
         {
-            var columnAsBinaryString = _column
-                .Replace("L", "0")
-                .Replace("R", "1");
-            return Convert.ToInt32(columnAsBinaryString, 2);
+            return FromStringWithBinary(_column, "L", "R");
+        }
+
+        private static int FromStringWithBinary(in string input, in string zero, in string one)
+        {
+            var inputAsBinaryString = input
+                .Replace(zero, "0")
+                .Replace(one, "1");
+            return Convert.ToInt32(inputAsBinaryString, 2);
         }
 
         public int Id()
