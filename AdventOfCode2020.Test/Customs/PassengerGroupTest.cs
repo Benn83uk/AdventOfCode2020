523e50b019dfa02c9626f6843e855ba3ff4de9f3
Ben Noble <ben@benshouse.net>
2020-12-06 07:30:36 +0000

    [DaySixTaskOne] BN - Reminder that we're dealing with a group of people, not a list

--- AdventOfCode2020.Test/Customs/PassengerGroupTest.cs
+++ AdventOfCode2020.Test/Customs/PassengerGroupTest.cs
@@ -9,36 +9,36 @@ namespace AdventOfCode2020.Test.Customs
         [Test]
         public void OnePersonOneAnswer()
         {
-            var list = new PassengerGroup("a");
-            Assert.That(list.CountUniqueAnswers(), Is.EqualTo(1));
+            var group = new PassengerGroup("a");
+            Assert.That(group.CountUniqueAnswers(), Is.EqualTo(1));
         }
         
         [Test]
         public void OnePersonTwoAnswers()
         {
-            var list = new PassengerGroup("ab");
-            Assert.That(list.CountUniqueAnswers(), Is.EqualTo(2));
+            var group = new PassengerGroup("ab");
+            Assert.That(group.CountUniqueAnswers(), Is.EqualTo(2));
         }
         
         [Test]
         public void TwoPeopleSameAnswers()
         {
-            var list = new PassengerGroup("abc", "abc");
-            Assert.That(list.CountUniqueAnswers(), Is.EqualTo(3));
+            var group = new PassengerGroup("abc", "abc");
+            Assert.That(group.CountUniqueAnswers(), Is.EqualTo(3));
         }
         
         [Test]
         public void TwoPeopleDifferentAnswers()
         {
-            var list = new PassengerGroup("abcx", "abcy");
-            Assert.That(list.CountUniqueAnswers(), Is.EqualTo(5));
+            var group = new PassengerGroup("abcx", "abcy");
+            Assert.That(group.CountUniqueAnswers(), Is.EqualTo(5));
         }
         
         [Test]
         public void ThreePeopleDifferentAnswers()
         {
-            var list = new PassengerGroup("abcx", "abcy", "abcz");
-            Assert.That(list.CountUniqueAnswers(), Is.EqualTo(6));
+            var group = new PassengerGroup("abcx", "abcy", "abcz");
+            Assert.That(group.CountUniqueAnswers(), Is.EqualTo(6));
         }
     }
 }
