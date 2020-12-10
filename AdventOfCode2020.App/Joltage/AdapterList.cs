2a9f7dab066f5770aef1a0d0c07961cdc8530cc6
Ben Noble <ben@benshouse.net>
2020-12-10 13:12:46 +0000

    [DayTenTaskTwo] BN - Have to admit I looked for hints on this one. What got me there was a combination of knowing gaps were 1 or 3, thinking that was somehow helpful, and an online comment about tribannaci - 1,1,2,4,7,13...

--- AdventOfCode2020.App/Joltage/AdapterList.cs
+++ AdventOfCode2020.App/Joltage/AdapterList.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode2020.App.Joltage
@@ -14,13 +15,13 @@ namespace AdventOfCode2020.App.Joltage
 
         public AdapterDifferences Chain()
         {
-            var orderedList = _adapters.Union(new int[]{0,DeviceRating()}).OrderBy(a => a).ToArray();
+            var orderedList = _adapters.Union(new int[] { 0, DeviceRating() }).OrderBy(a => a).ToArray();
             return AdapterDifferences(orderedList);
         }
 
         private static AdapterDifferences AdapterDifferences(int[] orderedList)
         {
-            var result = new AdapterDifferences(0,0,0);
+            var result = new AdapterDifferences(0, 0, 0);
             for (var i = 0; i < orderedList.Length - 1; i++)
             {
                 var diff = orderedList[i + 1] - orderedList[i];
@@ -37,30 +38,90 @@ namespace AdventOfCode2020.App.Joltage
 
         public long NumArrangements()
         {
-            var orderedList = _adapters.Union(new int[]{0,DeviceRating()}).OrderBy(a => a).ToArray();
-            return NumArrangements(orderedList);
+            var orderedList = _adapters.Union(new int[] { 0, DeviceRating() }).OrderBy(a => a).ToArray();
+            return NumArrangementsByPattern(orderedList);
         }
 
         private long NumArrangements(int[] orderedList)
         {
-            //Console.WriteLine($"Length: {orderedList.Length}");
             //Can only be one way to order 2 or less items
             if (orderedList.Length <= 2) return 1;
             if (orderedList.Length == 3)
             {
                 return ((orderedList[2] - orderedList[0] <= 3) ? 2 : 1);
             }
+
             return
                 ((orderedList[1] - orderedList[0] <= 3) ? NumArrangements(orderedList.Skip(1).ToArray()) : 0) +
                 ((orderedList[2] - orderedList[0] <= 3) ? NumArrangements(orderedList.Skip(2).ToArray()) : 0) +
                 ((orderedList[3] - orderedList[0] <= 3) ? NumArrangements(orderedList.Skip(3).ToArray()) : 0);
         }
-        
+
+        private long NumArrangementsByPattern(int[] orderedList)
+        {
+            //Look for groups where the difference between each is 1.
+            var groupLengths = new List<int>();
+            for (var i = 0; i < orderedList.Length; i++)
+            {
+                var currentGroup = 0;
+                for (var x = i; x < orderedList.Length-1; x++)
+                {
+                    Console.WriteLine($"Diff {orderedList[x + 1] - orderedList[x]}");
+                    if (orderedList[x + 1] - orderedList[x] == 1)
+                    {
+                        currentGroup++;
+                    }
+                    else
+                    {
+                        i = x;
+                        break;
+                    }
+                }
+                //Console.WriteLine($"Group of {currentGroup}");
+                groupLengths.Add(currentGroup);
+            }
+
+            long result = 1;
+            foreach (var length in groupLengths.Where(l => l > 1))
+            {
+                if (length == 2) result = result * 2;
+                else if (length == 3) result = result * 4;
+                else if (length == 4) result = result * 7;
+                else if (length == 5) result = result * 13;
+            }
+            return result;
+        }
+
         // (0) -> 2 -> 3 -> 5 -> (8)
         //                  5 -> (8) -- 1 way
         //             3 -> 5 -> 8 -- 1 way
         //        2 -> 3 -> 5 -> 8 -- 2 ways (2->3 or 2->5)
         // (0) -> 2 -> 3 -> 5 -> (8) -- 2 ways (0->2 or 0->3)
-        
+
+        //Differences
+        // (0) -> 1 -> 2 -> 3 -> 4 -> (7)
+        //      1    1    1    1   3
+        //     [1111 = 7] = 7
+
+        //Differences
+        // (0) -> 1 -> 4 -> (7)
+        //      1    3   3
+        //     [1 = 1] = 1
+
+        //Differences
+        // (0) -> 1 -> 4 -> 5 ->  (8)
+        //      1    3   1    3
+        //     [1 = 1] [1=1]       [Answer 1]
+
+
+        //Differences
+        // (0) -> 1 -> 4 -> 5 -> 6 ->  (9)
+        //      1    3   1    1    3
+        //     [1 = 1]   [11=3]    [Answer: 3]
+
+        //Differences
+        // (0) -> 1 -> 4 -> 5 -> 6 -> 9 -> 10 -> 11 -> (14)
+        //      1    3   1    1    3    1     1     3
+        //     [1 = 1]   [11=3]           [11=3]
     }
 }
--- AdventOfCode2020.Test/Joltage/AdapterListTest.cs
+++ AdventOfCode2020.Test/Joltage/AdapterListTest.cs
@@ -16,16 +16,16 @@ namespace AdventOfCode2020.Test.Joltage
             var result = adapters.Chain();
             Assert.That(result, Is.EqualTo(expected));
         }
-        
+
         [Test]
         public void CountOneDifferenceOfOneEach()
         {
             var expected = new AdapterDifferences(1, 1, 1);
-            var adapters = new AdapterList(1,3);
+            var adapters = new AdapterList(1, 3);
             var result = adapters.Chain();
             Assert.That(result, Is.EqualTo(expected));
         }
-        
+
         [Test]
         public void DayTenTaskOneExample()
         {
@@ -34,7 +34,7 @@ namespace AdventOfCode2020.Test.Joltage
             var result = adapters.Chain();
             Assert.That(result, Is.EqualTo(expected));
         }
-        
+
         [Test]
         public void DayTenTaskOneBigExampleFromFile()
         {
@@ -42,12 +42,12 @@ namespace AdventOfCode2020.Test.Joltage
             var numbers = lines.Select(int.Parse).ToArray();
             var adapters = new AdapterList(numbers);
             var result = adapters.Chain();
-            
+
             var expected = new AdapterDifferences(22, 0, 10);
             Assert.That(result, Is.EqualTo(expected));
             Assert.That(result.Checksum(), Is.EqualTo(220));
         }
-        
+
         [Test]
         public void DayTenTaskOneAnswer()
         {
@@ -55,10 +55,10 @@ namespace AdventOfCode2020.Test.Joltage
             var numbers = lines.Select(int.Parse).ToArray();
             var adapters = new AdapterList(numbers);
             var result = adapters.Chain();
-            
+
             Assert.That(result.Checksum(), Is.EqualTo(2775));
         }
-        
+
         [Test]
         public void OnlyOneWayToArrangeOneNumber()
         {
@@ -66,38 +66,27 @@ namespace AdventOfCode2020.Test.Joltage
             var result = adapters.NumArrangements();
             Assert.That(result, Is.EqualTo(1));
         }
-        
+
         [Test]
         public void TwoWays()
         {
-            var adapters = new AdapterList(1,2);
+            var adapters = new AdapterList(1, 2);
             var result = adapters.NumArrangements();
             Assert.That(result, Is.EqualTo(2));
         }
-        
-        [Test]
-        public void ThreeWays()
-        {
-            var adapters = new AdapterList(2,3,5);
-            // (0) -> 2 -> 3 -> 5 -> (8)
-            // (0) -> 2 -> 5 -> (8)
-            // (0) -> 3 -> 5 -> (8)
-            var result = adapters.NumArrangements();
-            Assert.That(result, Is.EqualTo(3));
-        }
-        
+
         [Test]
         public void OneWayLongChain()
         {
-            var adapters = new AdapterList(2,5,8);
+            var adapters = new AdapterList(2, 5, 8);
             var result = adapters.NumArrangements();
             Assert.That(result, Is.EqualTo(1));
         }
-        
+
         [Test]
         public void SevenWays()
         {
-            var adapters = new AdapterList(1, 2,3,4);
+            var adapters = new AdapterList(1, 2, 3, 4);
             // (0) -> 1 -> 2 -> 3 -> 4 -> (7)
             // (0) -> 1 -> 3 -> 4 -> (7)
             // (0) -> 1 -> 2 -> 4 -> (7)
@@ -108,7 +97,7 @@ namespace AdventOfCode2020.Test.Joltage
             var result = adapters.NumArrangements();
             Assert.That(result, Is.EqualTo(7));
         }
-        
+
         [Test]
         public void DayTenTaskTwoExample()
         {
@@ -116,7 +105,7 @@ namespace AdventOfCode2020.Test.Joltage
             var result = adapters.NumArrangements();
             Assert.That(result, Is.EqualTo(8));
         }
-        
+
         [Test]
         public void DayTenTaskTwoBigExampleFromFile()
         {
@@ -124,19 +113,113 @@ namespace AdventOfCode2020.Test.Joltage
             var numbers = lines.Select(int.Parse).ToArray();
             var adapters = new AdapterList(numbers);
             var result = adapters.NumArrangements();
-            
+
             Assert.That(result, Is.EqualTo(19208));
         }
+
+
         
-        
+        [Test]
         public void DayTenTaskTwoAnswer()
         {
             var lines = File.ReadAllLines("TestFiles/DayTenInput.txt");
             var numbers = lines.Select(int.Parse).ToArray();
             var adapters = new AdapterList(numbers);
             var result = adapters.NumArrangements();
-            
+
             Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void LongChainOneSections()
+        {
+            var adapters = new AdapterList(1, 4, 5, 6);
+            var result = adapters.NumArrangements();
+            Assert.That(result, Is.EqualTo(2));
+            //(0),1,4,5,6,(9)
+            //(0),1,4,  6,(9)
+        }
+
+        [Test]
+        public void LongChainTwoSections()
+        {
+            var adapters = new AdapterList(1, 4, 5, 6, 9, 10, 11);
+            var result = adapters.NumArrangements();
+            Assert.That(result, Is.EqualTo(4));
+            //  1  3 1 1 3 1  1  3  [1,2,2]
+            //(0),1,4,5,6,9,10,11,(14)
+            //(0),1,4,6,9,10,11,(14)
+            //(0),1,4,5,6,9,11,(14)
+            //(0),1,4,6,9,11,(14)
+        }
+
+        [Test]
+        public void LongerChainOneSections()
+        {
+            var adapters = new AdapterList(1, 4, 5, 6, 7);
+            var result = adapters.NumArrangements();
+            Assert.That(result, Is.EqualTo(4));
+            //(0),1,4,5,6,7,(10)
+            //(0),1,4,  6,7,(10)
+            //(0),1,4,5, ,7,(10)
+            //(0),1,4, , ,7,(10)
+        }
+
+        [Test]
+        public void EvenLongerChainOneSections()
+        {
+            var adapters = new AdapterList(1, 4, 5, 6, 7, 8);
+            var result = adapters.NumArrangements();
+            Assert.That(result, Is.EqualTo(7));
+            //(0),1,4,5,6,7,8,(11)
+            //(0),1,4,  6,7,8,(11)
+            //(0),1,4,5, ,7,8,(11)
+            //(0),1,4, , ,7,8,(11)
+            //(0),1,4,5,6, ,8,(11)
+            //(0),1,4, ,6, ,8,(11)
+            //(0),1,4,5, , ,8,(11)
+        }
+
+        [Test]
+        public void EvenEvenLongerChainOneSections()
+        {
+            var adapters = new AdapterList(1, 4, 5, 6, 7, 8, 9);
+            var result = adapters.NumArrangements();
+            Assert.That(result, Is.EqualTo(13));
+        }
+
+        [Test]
+        public void LongChainTwoSectionsOneLonger()
+        {
+            var adapters = new AdapterList(1, 4, 5, 6, 7, 10, 11, 12);
+            var result = adapters.NumArrangements();
+            Assert.That(result, Is.EqualTo(8));
+            //(0), 1, 4, 5, 6, 7, 10, 11, 12, (15)
+            //   1   3  1  1  1  3   1   1    3
+            //(0),1,4,5,6,7   ,X
+            //(0),1,4, ,6,7   ,X
+            //(0),1,4,5, ,7   ,X
+            //(0),1,4, , ,7   ,X
+            // A,             10,11,12,(15)
+            // A,             10,  ,12,(15)
+        }
+
+        [Test]
+        public void LongChainTwoSectionsBothLonger()
+        {
+            var adapters = new AdapterList(1, 4, 5, 6, 7, 10, 11, 12, 13);
+            var result = adapters.NumArrangements();
+            Assert.That(result, Is.EqualTo(16));
+            //(0), 1, 4, 5, 6, 7, 10, 11, 12, 13, (16)
+            //    1  3  1  1  1  3   1  1    1   3
+            //(0),1,4,5,6,7   ,X
+            //(0),1,4, ,6,7   ,X
+            //(0),1,4,5, ,7   ,X
+            //(0),1,4, , ,7   ,X
+            // A,             10,11,12,13,(16)
+            // A,             10,  ,12,13,(16)
+            // A,             10,11,  ,13,(16)
+            // A,             10,  ,  ,13,(16) 
         }
     }
 }
